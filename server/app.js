82367015a007ed8e8b6b4fb626f5e16c84413247
yjcho <jinjanic91@gmail.com>
2020-12-28 23:01:18 +0900

    model 구성하기

--- server/app.js
+++ server/app.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import config from './config';
 
-const app = express()
+const app = express();
+const {MONGO_URI} = config;
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("MONGO DB CONNECTED!!!"))
+.catch((e) => console.log(e));
 
 app.get('/')
 
