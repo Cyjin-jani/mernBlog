fcc48943af9a643463a6fc689ba93171d0141dd3
yjcho <jinjanic91@gmail.com>
2021-01-01 21:14:48 +0900

    header 세팅 시작

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import {Row, Col} from 'reactstrap';
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div id="page-header" className="mb-3">
+            <Row>
+                <Col md="6" sm="auto" className="text-center m-auto">
+                    <h1>Read Our Blog</h1>
+                    <p> My side Project Blog</p>
+                </Col>
+            </Row>
         </div>
     )
 }
