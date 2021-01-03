0397e1cffd3dbe67676d408ca38bbec3719b04a0
yjcho <jinjanic91@gmail.com>
2021-01-03 18:46:06 +0900

    로그인 modal 완료

--- client/src/redux/sagas/index.js
+++ client/src/redux/sagas/index.js
@@ -1,6 +1,16 @@
-import {all} from 'redux-saga/effects';
+import {all, fork} from 'redux-saga/effects';
+import axios from 'axios';
+
+import authSaga from './authSaga';
+import dotenv from 'dotenv';
+dotenv.config()
+
+axios.defaults.baseURL = process.env.REACT_APP_BASIC_SERVER_URL;
+
 
 //여러 값을 반환할 수 있게 되어있는 function*
 export default function* rootSaga() {
-    yield all([]);
+    yield all([
+        fork(authSaga)
+    ]);
 }
